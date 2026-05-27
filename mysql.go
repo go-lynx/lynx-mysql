edf89dbb9fb416333b5ddf25a4a200e62aae8399
tan-zhuo <tanzhuo@aliyun.com>
2026-05-27 22:12:47 +0900

    chore: bump pluginVersion to v1.6.1

--- mysql.go
+++ mysql.go
@@ -23,7 +23,7 @@ import (
 // Plugin metadata
 const (
 	pluginName        = "mysql.client"
-	pluginVersion     = "v1.5.4"
+	pluginVersion     = "v1.6.1"
 	pluginDescription = "mysql client plugin for lynx framework"
 	confPrefix        = "lynx.mysql"
 )
